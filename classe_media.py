22b00da839393d7653ac1e60043f9db553fbee7b
Gabriel Laxy <127798119+GabrielLaxy@users.noreply.github.com>
2024-04-22 15:02:25 -0300

    Delete classe_media.py

--- classe_media.py
+++
@@ -1,9 +0,0 @@
-class Calculadora_de_media:
-
-    @staticmethod
-    def media(x,y):
-        return (x+y)/2
-    
-def test_add(self):
-  result = Calculadora_de_media.media(5, 3)
-  assert result == 4
